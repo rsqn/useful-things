877756c43abd01971236dde343837a4b3b64024d
Michael Andrewes <mandrewes@roguesquadron.com.au>
2017-08-04 14:07:53 +1000

    changed-regisration-area

--- simple-utilities/src/main/java/tech/rsqn/useful/things/util/TagReplacer.java
+++ simple-utilities/src/main/java/tech/rsqn/useful/things/util/TagReplacer.java
@@ -5,12 +5,6 @@ import java.util.Map;
 
 public class TagReplacer {
 
-    /**
-     * This exists only to make the message routing spring configuration a bit easier to follow. (its not "fast")
-     * @param tpl
-     * @param tags
-     * @return
-     */
     public static String replaceTags(String tpl, Map<String,String> tags) {
         String ret = tpl;
         for (String s : tags.keySet()) {
@@ -23,7 +17,7 @@ public class TagReplacer {
         Map<String,String> m = new HashMap<>();
 
         if ( tags.length %2 != 0) {
-            throw new RuntimeException("Tags list must be a multiple of 2 as it is converted into a map");
+            throw new RuntimeException("Tags list must be a multiple of 2. It is converted into a map");
         }
 
         for (int i = 0; i < tags.length ; i+=2) {
