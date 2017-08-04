40d1f7defcce76a2a9e5a0020ec294f970c70f6e
Michael Andrewes <mandrewes@roguesquadron.com.au>
2017-08-04 12:25:15 +1000

    upgraded-servlet-api

--- application-utilities/src/main/java/tech/rsqn/useful/things/systems/AWSClientFactory.java
+++ application-utilities/src/main/java/tech/rsqn/useful/things/systems/AWSClientFactory.java
@@ -2,6 +2,7 @@ package tech.rsqn.useful.things.systems;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.RegionUtils;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagementClient;
 import org.springframework.beans.factory.InitializingBean;
@@ -11,6 +12,7 @@ public class AWSClientFactory implements InitializingBean {
 
     private String accessKey;
     private String secretKey;
+    private String region;
 
     @Required
     public void setAccessKey(String accessKey) {
@@ -22,16 +24,21 @@ public class AWSClientFactory implements InitializingBean {
         this.secretKey = secretKey;
     }
 
+    @Required
+    public void setRegion(String region) {
+        this.region = region;
+    }
 
     public AmazonS3Client s3Client() {
         AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-        AmazonS3Client s3client = new AmazonS3Client(credentials);
-        return s3client;
+        AmazonS3Client client = new AmazonS3Client(credentials);
+        return client;
     }
 
     public AWSSimpleSystemsManagementClient systemsManagementClient() {
         AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
         AWSSimpleSystemsManagementClient client = new AWSSimpleSystemsManagementClient(credentials);
+        client.setRegion(RegionUtils.getRegion(region));
         return client;
     }
 
