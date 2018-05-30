4d8ce169bfa39f372d602968a16c586383d603ed
Michael Andrewes <mandrewes@gmail.com>
2018-05-30 19:50:54 +1000

    randomstring

--- simple-utilities/src/main/java/tech/rsqn/useful/things/util/RandomUtil.java
+++ simple-utilities/src/main/java/tech/rsqn/useful/things/util/RandomUtil.java
@@ -1,9 +1,63 @@
 package tech.rsqn.useful.things.util;
 
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.Random;
 
+/**
+ * Created with IntelliJ IDEA.
+ * User: mandrewes
+ * Date: 4/18/13
+ * Time: 7:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
 public class RandomUtil {
-    private static Random g_rn = new Random(System.currentTimeMillis());
+    private static SecureRandom g_rn = new SecureRandom(new Date().toString().getBytes());
+
+    public static String getRandomString(int length) {
+        int lo = 48;
+        int hi = 122;
+        String ret = "";
+        for (int i = 0; i < length; i++) {
+            int value = getRange(lo, hi);
+            if (acceptableChar(value)) {
+                ret += (new Character((char) value).toString());
+            } else {
+                i--;
+            }
+        }
+        return ret;
+
+    }
+
+    public static boolean acceptableChar(int value) {
+        if (value >= 48 && value <= 57)
+            return true;
+
+        if (value >= 65 && value <= 90)
+            return true;
+
+        if (value >= 97 && value <= 122)
+            return true;
+
+        return false;
+
+    }
+
+    public static byte[] getRandomBytes(int length) {
+        int lo = 0;
+        int hi = 255;
+
+        byte[] ret = new byte[length];
+
+        for (int i = 0; i < length; i++) {
+            int value = getRange(lo, hi);
+            ret[i] = (byte) value;
+        }
+        return ret;
+
+    }
 
     public static int getRange(int lo, int hi) {
         if (lo > hi) {
