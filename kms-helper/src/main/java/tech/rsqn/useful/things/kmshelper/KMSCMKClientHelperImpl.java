f79cd60ee8773b099e0b4fe957c68af46eba8787
Colin Nevin <colin.nevin@isignthis.com>
2020-08-21 20:47:24 +1000

    check in

--- kms-helper/src/main/java/tech/rsqn/useful/things/kmshelper/KMSCMKClientHelperImpl.java
+++ kms-helper/src/main/java/tech/rsqn/useful/things/kmshelper/KMSCMKClientHelperImpl.java
@@ -1,9 +1,12 @@
 package tech.rsqn.useful.things.kmshelper;
 
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.kms.AWSKMS;
 import com.amazonaws.services.kms.AWSKMSClientBuilder;
 import com.amazonaws.services.kms.model.DecryptRequest;
+import com.amazonaws.services.kms.model.DecryptResult;
 import com.amazonaws.services.kms.model.EncryptRequest;
+import com.amazonaws.services.kms.model.EncryptResult;
 
 import java.nio.ByteBuffer;
 
@@ -16,22 +19,23 @@ public class KMSCMKClientHelperImpl implements KMSCMKClientHelper {
     @Override
     public byte[] encrypt(byte[] plainText) {
 
-        ByteBuffer plaintext = ByteBuffer.wrap(plainText);
+        ByteBuffer wrappedPlainText = ByteBuffer.wrap(plainText);
 
-        EncryptRequest req = new EncryptRequest().withKeyId(cmkKeyIdArn).withPlaintext(plaintext);
-        ByteBuffer ciphertext = kmsClient().encrypt(req).getCiphertextBlob();
+        EncryptRequest request = new EncryptRequest().withKeyId(cmkKeyIdArn).withPlaintext(wrappedPlainText);
+        EncryptResult result = kmsClient().encrypt(request);
 
-        return ciphertext.array();
+        return result.getCiphertextBlob().array();
     }
 
     @Override
     public byte[] decrypt(byte[] cryptText) {
-        ByteBuffer ciphertextBlob = ByteBuffer.wrap(cryptText);
+        ByteBuffer wrappedCryptText = ByteBuffer.wrap(cryptText);
 
-        DecryptRequest req = new DecryptRequest().withKeyId(cmkKeyIdArn).withCiphertextBlob(ciphertextBlob);
-        ByteBuffer plainText = kmsClient().decrypt(req).getPlaintext();
-        return plainText.array();
+        DecryptRequest request = new DecryptRequest().withCiphertextBlob(wrappedCryptText);
+        DecryptResult result = kmsClient().decrypt(request);
+        return result.getPlaintext().array();
     }
+
 
     public KMSCMKClientHelperImpl(String keyIdArn) {
         this.cmkKeyIdArn = keyIdArn;
@@ -39,7 +43,8 @@ public class KMSCMKClientHelperImpl implements KMSCMKClientHelper {
 
     private AWSKMS kmsClient() {
         if (kmsClient == null) {
-            kmsClient = AWSKMSClientBuilder.standard().build();
+            kmsClient =
+                AWSKMSClientBuilder.standard().withCredentials(DefaultAWSCredentialsProviderChain.getInstance()).build();
         }
         return kmsClient;
     }
